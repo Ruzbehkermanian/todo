0fba529bfb575ab36a1a5b80912f7ac109f8ba45
Ruzbehkermanian <rk592@njit.edu>
2017-05-05 19:28:27 -0400

    Logout function made

---
+++ logout.php
@@ -0,0 +1,13 @@
+
+<!--/**
+ * Created by PhpStorm.
+ * User: ruzbehkermanian
+ * Date: 5/5/17
+ * Time: 3:52 PM
+ **/-->
+
+
+  <form action = "login.php">
+  <input type="submit" value ="Logout"/>
+  </form>
+
