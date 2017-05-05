ce50bc6b9f564e22204e626f230bf6b6d5340c0e
Ruzbehkermanian <rk592@njit.edu>
2017-05-05 16:46:55 -0400

    Fixing the login system

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+ 
+
+
+<div class="login_text">
+ <h1> Login Page</h1> </div>
+ <link rel="stylesheet" type = "text/css" href="./main.css">
+
+
+ <div class ="login">
+ <form method = "post" action ="index.php">
+ <strong class="text-white">username:</strong><input type = "test" name="reg_uname" value=""/><br><br>
+ <strong>password:</strong><input type = "password" name="reg_password" value=""/><br><br>
+
+
+ <input type="hidden" name="action" value="test_user">
+     <input type="submit" value ="Login">
+ </form>
+
+ <form action = "register.php"> <input type="submit" value ="Register">
+ </form>
+ </div>
+
+</body>
+</html>
+
