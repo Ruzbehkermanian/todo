6cabe55a646c2d287294ddf321a2d60a446231d5
Ruzbehkermanian <rk592@njit.edu>
2017-05-06 17:06:58 -0400

    edit function added

---
+++ editdata.php
@@ -0,0 +1,23 @@
+
+
+<html>
+<head>
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="login-page">
+    <div class="form">
+        <form class="login-form" action="index.php" method="post">
+            <input  type='text'name='todo_item' placeholder="Todo Item" maxlength="40"/>
+            <input  type='text' name='description' placeholder="Description"/>
+            <input type="date" name="date_of_entry" placeholder="Date of Entry"/>
+            <input type='time'name='time_of_entry' value="" placeholder="Time of Entry"/>
+
+            <input type="hidden" name="action" value="Edit">
+            <button>Edit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
