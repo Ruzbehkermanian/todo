15e070518a2c9ff6e86bfdbda98918a30cb086e8
Ruzbehkermanian <rk592@njit.edu>
2017-05-06 22:55:16 -0400

    Removed error and making it work

--- add_description.php
+++ add_description.php
@@ -9,13 +9,12 @@
 <div class="login-page">
     <div class="form">
         <form class="login-form" action="index.php" method="post">
-            <input input type='text'name='todo_item' placeholder="Todo Item"/>
-            <input input type='text' name='description' placeholder="Description"/>
-            <input type="date" name="date_of_entry" placeholder="Date of Entry"/>
+            <input  type='text'name='todo_item' placeholder="Todo Item" maxlength="40"/>
+            <input  type='text' name='description' placeholder="Description"/>
+            <input type="date" name='date_of_entry' placeholder="Date of Entry"/>
             <input type='time'name='time_of_entry' value="" placeholder="Time of Entry"/>
 
-            <input type="hidden" name="action" value="add">
-            <!--<input type="submit" class="button" value="Add"/>-->
+            <input type="hidden" name="action" value="Add">
         <button>Add</button>
         </form>
     </div>
