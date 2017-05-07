f1f0e931b0bf0bce4a1d670f3cca57e1bdf8f67c
Ruzbehkermanian <rk592@njit.edu>
2017-05-07 15:49:36 -0400

    Error file uploaded for login

---
+++ login_error.php
@@ -0,0 +1,25 @@
+
+
+<html>
+<head>
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="login-page">
+    <div class="form">
+        <p class="message">Correct Username, Wrong password</p>
+        <form class="login-form" action="index.php" method="post">
+            <input type="text" name="reg_uname" placeholder="username" required/>
+            <input type="password" name="reg_password" placeholder="password" required/>
+            <input type="hidden" name="action" value="test_user">
+            <button>login</button>
+            <p class="message">Not registered? <a href="register.php">Create an account</a></p>
+        </form>
+    </div>
+</div>
+</body>
+</html>
+
+
+
