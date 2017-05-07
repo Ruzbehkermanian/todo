9ff46c0d4a08118e7fbf5ca14a9cf4f9ff4f46fb
Ruzbehkermanian <rk592@njit.edu>
2017-05-06 22:55:41 -0400

    Editing the file and solving the error

--- editdata.php
+++ editdata.php
@@ -6,18 +6,23 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
+<?php foreach ($result as $res):?>
+
+
 <div class="login-page">
     <div class="form">
         <form class="login-form" action="index.php" method="post">
-            <input  type='text'name='todo_item' placeholder="Todo Item" maxlength="40"/>
-            <input  type='text' name='description' placeholder="Description"/>
-            <input type="date" name="date_of_entry" placeholder="Date of Entry"/>
-            <input type='time'name='time_of_entry' value="" placeholder="Time of Entry"/>
-
+            <input  type='text'name='todo_item' value="<?php echo $res['todo_item'];?>" maxlength="40"/>
+            <input  type='text' name='description' value="<?php echo $res['description'];?>">
+            <input type="date" name="date_of_entry" value="<?php echo $res['date_of_entry'];?>">
+            <input type='time'name='time_of_entry' value="<?php echo $res['time_of_entry'];?>">
             <input type="hidden" name="action" value="Edit">
             <button>Edit</button>
         </form>
+
+        <?php endforeach ;?>
     </div>
 </div>
 </body>
 </html>
+
