e32333456a985899d9cf6b7fbf0e093dae505ed1
Ruzbehkermanian <rk592@njit.edu>
2017-05-08 18:20:03 -0400

    Minor errors resolved in edit file

--- editdata.php
+++ editdata.php
@@ -12,17 +12,19 @@
 <div class="login-page">
     <div class="form">
         <form class="login-form" action="index.php" method="post">
-            <input  type='text'name='todo_item' value="<?php echo $res['todo_item'];?>" maxlength="40"/>
-            <input  type='text' name='description' value="<?php echo $res['description'];?>">
+            <input type="text" name='todo_item' value="<?php echo $res['todo_item'];?>" maxlength="40"/>
+            <input type="text" name='description' value="<?php echo $res['description'];?>">
             <input type="date" name="date_of_entry" value="<?php echo $res['date_of_entry'];?>">
-            <input type='time'name='time_of_entry' value="<?php echo $res['time_of_entry'];?>">
+            <input type="time" name="time_of_entry" value="<?php echo $res['time_of_entry'];?>">
+            <input type="hidden" name="ID" value="<?php echo $res['id'];?>">
             <input type="hidden" name="action" value="Edit">
             <button>Edit</button>
         </form>
 
-        <?php endforeach ;?>
+        <?php endforeach;?>
     </div>
 </div>
 </body>
 </html>
+
 
