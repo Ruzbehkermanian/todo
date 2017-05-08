c368b4fba9c3b8fd4bcadcbad35b40d65bdb602f
Ruzbehkermanian <rk592@njit.edu>
2017-05-08 18:21:18 -0400

    Logout function edited

--- logout.php
+++ logout.php
@@ -1,18 +1,12 @@
 
-<!--/**
- * Created by PhpStorm.
- * User: ruzbehkermanian
- * Date: 5/5/17
- * Time: 3:52 PM
- **/-->
 
   <html>
   <head>
+  </head>
       <body>
   <form action = "login.php">
-      <input type="submit" value ="Logout"/>
+      <input type="submit" value ="Logout" class="button"/>
   </form>
   </body>
-  </head>
   </html>
 
