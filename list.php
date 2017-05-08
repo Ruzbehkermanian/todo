d04ae0141539a5eda2ac6e739c676984e49181b2
Ruzbehkermanian <rk592@njit.edu>
2017-05-07 21:02:40 -0400

    Modified the page

--- list.php
+++ list.php
@@ -1,38 +1,29 @@
-<?php
 
-  echo "<h2> To Do list system </h2>";
-  echo " <h3> Welcome, ". $_COOKIE['login'] .' <br/>';
-  echo " <h3> Below you may find your to-do items </h3><br/><br/>";
-?>
 
 <html>
 <head>
-   <link rel="stylesheet" href="main.css">
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            background-color: #f1f1c1;
-        }
-    </style>
 
-    <button class='btn btn-primary pull-right'>
-        <form align="right" action="login.php">
-            <input type="submit" value="Logout" class="button"> </form></button>
+    <form align="right" action="login.php">
+        <input type="submit" value="Logout" class="button"> </form>
+    <?php
+   // echo "<h2> To Do list system  </h2><br/><br/>";
+
+
+    echo " <h3> Welcome, ". $_COOKIE['login'] .' <br/>';
+    echo " <h3> Below you may find your to-do items </h3><br/><br/>";
+        ?>
+
+    <form action='add_description.php'>
+        <label> Add a new item</label>
+        <input type="submit" value="Add" align=""/>
+    </form>
+
+   <link rel="stylesheet" href="main.css">
 
 </head>
 
 <body>
 
-
-<tr>
-    <td>
-
-        <form class=".top">
-            <h2>To Do List System</h2>
-        </form>
-    </td>
-</tr>
-<header> </header>
 
 <table style="width:80%">
 
@@ -49,33 +40,26 @@
               <td><?php echo $res['description']. '</br>'; ?>'</td>
               <td><?php echo $res['date_of_entry']. '</br>'; ?>'</td>
               <td><?php echo $res['time_of_entry']. '</br>'; ?>'</td>
+              <td> <form action='index.php' method='post'>
+                  <input type='hidden' name='edit_id' value='<?php echo $res['id']; ?>'>
+                  <input type='hidden' name='action' value='Edit'; >
+                  <input type='submit' value="Edit">
+                  </form></td>
+
+              <td>
+                  <form action='index.php' method='post'>
+                      <input type='hidden' name='item_id' value='<?php echo $res['id'] ?>'>
+                      <input type='hidden' name='action' value='delete'; >
+                      <input type='submit' value="delete">
+                  </form>
+              </td>
 
           </tr>
 
-          <td>
-              <form action='index.php' method='post'>
-                  <input type='hidden' name='edit_id' value='<?php echo $res['id']; ?>'>
-                  <input type='hidden' name='action' style="float: right" value='Edit'; >
-                  <input type='submit' value="Edit">
-              </form>
-          </td>
-
-          <td>
-	   <form action='index.php' method='post'>
-	   <input type='hidden' name='item_id' value='<?php echo $res['id'] ?>'>
-	   <input type='hidden' name='action' style="float: right" value='delete'; >
-	   <input type='submit' value="delete">
-	   </form>
-	     </td>
 
 <?php endforeach; ?>
 
 </table>
-
-   <form action='add_description.php'>
-   <label> Add a new item</label>
-    <input type="submit" value="Add"/>
-    </form>
 </body>
 </html>
 
